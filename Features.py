5469f3ffa218bf1a0715a6108f59b11255d27761
Shyam-Khokhariya <shyamkhokhariya@gmail.com>
2018-12-08 19:09:46 +0530

    1 commit

---
+++ Features.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+train=pd.read_csv("/home/ghanshyam/Machine Learning/PubG/train_V2.csv")
+train.info()
